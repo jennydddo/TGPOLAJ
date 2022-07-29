69a580aa93c2a618d0ec2fbe8e61f30d86b4922a
jennydddo <jennydddo.n@gmail.com>
2022-07-29 20:21:39 +0700

    platform

--- main.py
+++ main.py
@@ -12,6 +12,53 @@ class GameSprite(sprite.Sprite):
     def update(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
 
+shift = 0 
+speed = 0
+for event in event.get():
+    if event.type == KEYDOWN: 
+        if event.key == K_d:
+            speed = 5 
+        elif event.key == K_a: 
+            speed = -5 
+    elif event.type == KEYUP: 
+       if event.key == K_d:
+            speed = 0 
+        elif event.key == K_a: 
+            speed = 0
+shift += speed  
+local_shift = shift % width
+window.blit("background",(local_shift,0))
+if local_shift != 0: 
+    window.blit("background",(local_shift - width,0))
+width = 1200
+left_bound = width / 40 
+right_bound = width - left_bound
+if screen.rect.x > right_bound or screen.rect.x < left_bound: 
+    screen.rect.x -= shift 
+
+class Platform(sprite.Sprite): 
+    def __init__(self,x,y,w,h,color): 
+        super().__init__()
+        self.image = Surface((w,h))
+        self.rect = self.image.get_rect()
+        self.image.fill(color)
+        self.rect.x = x
+        self.rect.y = y
+        self.color = color
+    def draw(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+class MovingPlatform(Platform): 
+    changex = 0 
+    chagey = 0 
+    boundary_top = 0
+    boundary_bottom = 0
+    boundary_left = 0
+    boundary_right = 0
+    player = None
+def update(self):
+    self.rect.x += self.change_x
+
+    
 window = display.set_mode((1000,670))
 background = transform.scale(image.load("scifi background.webp"),(1000,670))
 clock = time.Clock()
